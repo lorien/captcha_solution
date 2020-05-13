511fa817bf652aec330bc5448d0925f63b677d72
Gregory Petukhov <lorien@lorien.name>
2020-05-14 00:33:24 +0300

    Bump version: 0.0.2 → 0.0.3

--- captcha_solution/__init__.py
+++ captcha_solution/__init__.py
@@ -1,4 +1,4 @@
 from .solver import CaptchaSolver
 from .error import * 
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name = 'captcha-solution',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
     maintainer='Gregory Petukhov',
